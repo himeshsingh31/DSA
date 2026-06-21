97bbe8cd657f33747ad05c02213d7e2dd05808cf
himeshsingh <himeshsolanki3156@gmail.com>
2026-06-21 23:51:17 +0530

    final revision of linked list

---
+++ linked list/fianl.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+class node{
+public:
+int data;
+node* next;
+
+node(int data){
+this->data = data;
+this->next = NULL;
+}
+
+};
+
+void attail(int data,node* &tail){
+
+    node* x = new node(data);
+tail->next = x;
+tail = x;
+
+
+
+}
+
+
+int main(){
+int x;
+cout<<"enter the first value of the ll to start it ."<<endl;
+cin>>x;
+    node* first = new node(x);
+
+attail(20,first);
+attail(30,first);
+attail(40,first);
+attail(50,first);
+attail(60,first);
+attail(70,first);
+attail(80,first);
+
+
+}
