74362c0253b785ebf5f4d0bd68d0054521d95ee5
himeshsingh <himeshsolanki3156@gmail.com>
2026-06-24 00:39:03 +0530

    deleter

--- heap/finalrev.cpp
+++ heap/finalrev.cpp
@@ -16,34 +16,68 @@ public:
     void maker(int data)
     {
         size += 1;
-        int largest = size;
+        int child = size;
         arr[size] = data;
 
-        while (size > 0)
+        while (child > 1)
         {
             // for max heap
-            int parent = largest / 2;
-            if (arr[size / 2] < arr[size])
+            int parent = child / 2;
+            if (arr[parent] < arr[child])
             {
-                swap(arr[size], arr[size / 2]);
-                largest = parent;
+                swap(arr[parent], arr[child]);
+                child = parent;
             }
             else
-                return;
+                break;
         }
+    }
+
+    void deleter()
+    {
+        // for max heap
+        swap(arr[1], arr[size]);
+        size -= 1;
+        int largest = 1;
+        while (largest < size)
+        {
+      
+            int child = largest*2;
+      int rightchild =(largest *2) +1;
+            if (child<= size && arr[largest] < arr[child] )
+            {swap(arr[largest],arr[child]);
+                largest = child;
+
+            }
+            if (rightchild<= size &&  arr[largest] < arr[rightchild] )
+            {swap(arr[largest], arr[rightchild]);
+                largest = child + 1;
+            }
+
+        }
+
+        return;
+        
+    }
+
+    void printer(){
+      for(int i =1;i<=size;i++){
+        cout<<arr[i]<<' ';
+      }
     }
 };
 
 int main()
 {
     heap h;
-
     h.maker(10);
     h.maker(20);
     h.maker(30);
     h.maker(40);
     h.maker(50);
     h.maker(60);
+
+    h.printer();
 
     return 0;
 }
--- heap/tempCodeRunnerFile.cpp
+++ heap/tempCodeRunnerFile.cpp
@@ -1,2 +1,86 @@
-h.heapify(arr2,9);
-//     h.printheap(arr2,9);
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class heap
+{
+public:
+    int arr[100];
+    int size = 0;
+    heap()
+    {
+        arr[0] = -1;
+    }
+
+    void maker(int data)
+    {
+        size += 1;
+        int child = size;
+        arr[size] = data;
+
+        while (size > 0)
+        {
+            // for max heap
+            int parent = child / 2;
+            if (arr[parent] < arr[child])
+            {
+                swap(arr[parent], arr[child]);
+                child = parent;
+            }
+            else
+                break;
+        }
+    }
+
+    void deleter()
+    {
+        // for max heap
+        swap(arr[1], arr[size]);
+        size -= 1;
+        int largest = 1;
+        while (largest < size)
+        {
+      
+            int child = largest/2;
+      int rightchild =(largest /2) +1;
+            if (arr[largest] < arr[child] && child <= size)
+            {swap(arr[largest],arr[child]);
+                largest = child;
+
+            }
+            if (arr[largest] < arr[rightchild] && rightchild<= size)
+            {swap(arr[largest], arr[rightchild]);
+                largest = child + 1;
+            }
+
+        }
+
+        return;
+        
+    }
+
+    void printer(){
+        int x = sizeof(arr)/sizeof(int);
+        int i =1;
+        while(i<=x){
+            cout <<arr[i]<<"- -";
+            i++;
+        }
+    }
+};
+
+int main()
+{
+    heap h;
+    h.maker(10);
+    h.maker(20);
+    h.maker(30);
+    h.maker(40);
+    h.maker(50);
+    h.maker(60);
+
+    h.printer();
+
+    return 0;
+}
