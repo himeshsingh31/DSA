eee436e62ceb977ee9f8cb9da50924e8cbc5cda8
himeshsingh <himeshsolanki3156@gmail.com>
2026-07-07 14:49:26 +0530

    topologixal sort by dfs and bfs 7july26

--- Graph/final_rev.cpp
+++ Graph/final_rev.cpp
@@ -208,9 +208,83 @@ unordered_map<int,bool>dfstr;
 
 /// TOPOLOGICAL SORT BY DFS ..
 
+void dfs(int node,vector<bool>&visited,stack<int>&s,unordered_map<int,list<int>>&adj){
+visited[node] = true;
+
+for(auto i:adj[node]){
+    if(!visited[i]){
+        dfs(i,visited,s,adj);
+    }
+}
+
+s.push(node);
+
+
+}
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+unordered_map<int,list<int>>adj;
+vector<bool>visited(v,false);
+for(int i=0;i<e;i++){
+    int u = edges[i][0];
+    int v = edges[i][1];
+
+    adj[u].push_back(v);
+}
+stack<int>s;
+for(int i=0;i<v;i++){
+    if(!visited[i]){
+        dfs(i,visited,s,adj);
+    }
+}
+vector<int>ans;
+while(!s.empty()){
+ans.push_back(s.top());
+s.pop();
+}
+return ans;
+}
+
 
 /// TOPOLOGICAL SORT BY BFS (KAHN'S ALGORITHM)
+#include <bits/stdc++.h> 
 
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+unordered_map<int,list<int>>adj;
+vector<int>indegree(v,0);
+
+for(int i=0;i<e;i++){
+    int u = edges[i][0];
+    int v = edges[i][1];
+
+    adj[u].push_back(v);
+indegree[v]++;
+
+}
+queue<int>q;
+
+for(int i =0;i<v;i++){
+    if(indegree[i]==0){
+        q.push(i);
+    }
+}
+vector<int>ans;
+while(!q.empty()){
+    int x = q.front();
+    q.pop();
+    ans.push_back(x);
+    for(auto i:adj[x]){
+indegree[i]--;
+if(indegree[i]==0)
+q.push(i);
+    }
+}
+
+
+return ans;
+}
 
 
 ///CYCLE DETECTION IN DIRECTED GRAPH BY BFS (KAHN'S ALGORITHM)
